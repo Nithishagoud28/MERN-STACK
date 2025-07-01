c25e80e8539201ef20fff4ef83b25f2d1d56977e
Nithishagoud28 <nithishagoud45@gmail.com>
2025-07-01 14:48:58 +0530

    updatedall

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,11 +20,11 @@ function App() {
       <div>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/RegisterForm" element={<RegisterForm />} />
-        <Route path="/Herosection" element={<HeroSection />} />
-        <Route path= "/Courses" element= { <Courses/>} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/herosection" element={<HeroSection />} />
+        <Route path= "/courses" element= { <Courses/>} />
         <Route path="/placements" element={<Placements />} />
-        < Route path='/Batches' element = {< Batches />} />
+        <Route path="/batches" element={<Batches />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         {/* <Route path="/Loginform" element={<LoginForm />} /> */}
--- Frontend/src/Components/Courses/PopularCourses.jsx
+++ Frontend/src/Components/Courses/PopularCourses.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./popularcourses.css";
+import "./PopularCourses.css";
+
 
 const courses = [
   {
